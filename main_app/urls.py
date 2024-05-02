caf3ca528f79af9c7725989de0023ae002db6939
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-05-02 15:50:24 -0700

    Add login/logout functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('home')
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {})
