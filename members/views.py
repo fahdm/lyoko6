833903ffa80fd98a6cfcc7ba911f9dfb0b1b912e
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-05-03 09:24:04 -0700

    Cleaning up Code, implement authorization

--- members/views.py
+++ members/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import login
+
+def home(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    
+    context = {
+        'form': form,
+        
+    }
+    return render(request, 'home.html', context)
 
 
 class UserRegisterView(generic.CreateView):
